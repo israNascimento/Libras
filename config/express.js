0aa80502b6bf8b6e08c1ffd204e7a64ea756969a
Israel Nascimento <dev.israelnascimento@gmail.com>
2017-12-02 17:13:15 -0200

    Configurando e iniciando views

--- config/express.js
+++ config/express.js
@@ -14,16 +14,15 @@ module.exports = function () {
     //app.use(expressValidator());
 
     load('routes', { cwd: 'app', verbose: true })
-        .then('infra')
         .into(app);
-
+/*
     app.use(function (req, res, next) {
         res.status(404).render("erros/404");
     });
 
     app.use(function (error, req, res, next) {
         res.status(500).render("erros/500");
-    });
+    });*/
     //tem que colocar na ordem, caso contrário ele passa pelo middleware e ainda não vai ter acontecido nenhum erro.
     return app;
 };
