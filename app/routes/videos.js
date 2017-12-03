5ce17a9966e75c2c28f4b2343ddbd7beb60b6fab
Israel Nascimento <dev.israelnascimento@gmail.com>
2017-12-03 19:20:18 -0200

    Página de vídeos

--- app/routes/videos.js
+++ app/routes/videos.js
@@ -1,6 +1,19 @@
 module.exports = function(app) {
-    app.get("/video", function(req, res) {
-        res.render("video.ejs");
+    app.get("/videos/categoria/:categoria", function(req, res) {
+        if (!req.session.user) {
+            res.redirect("/");
+            return;
+        }
+        var categoria = req.params.categoria;
+        var connection = app.infra.connectionFactory();
+        var dao = new app.infra.VideoDAO(connection);
+        dao.lista(categoria, function(err, result) {
+            if(err) {
+                console.log(err);
+                return;
+            }
+            res.render("video.ejs", {videos: result});
+        });
     });
 
     app.get("/videos/upload", function(req, res) {
@@ -8,6 +21,7 @@ module.exports = function(app) {
             res.redirect("/");
             return;
         }
+        console.log("get");
         var connection = app.infra.connectionFactory();
         var dao = new app.infra.VideoDAO(connection);
         dao.categorias(function(err, result) {
@@ -24,18 +38,29 @@ module.exports = function(app) {
             res.redirect("/");
             return;
         }
-        console.log(req.body);
         var video = req.body;
+        video["link"] = youtubeId(video.link);
+        console.log(youtubeId(video.link));
         video['usuario'] = req.session.user;
         var connection = app.infra.connectionFactory();
         var dao = new app.infra.VideoDAO(connection);
         dao.upload(video, function(err) {
             if(err) {
-                console.log(err);
                 res.redirect("/videos/upload");
                 return;
             }
             res.redirect("/dashboard");
         });
     });
+
+    function youtubeId(link) {
+        var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+        var match = link.match(regExp);
+
+        if (match && match[2].length == 11) {
+            return match[2];
+        } else {
+            return 'error';
+        }
+    }
 }
